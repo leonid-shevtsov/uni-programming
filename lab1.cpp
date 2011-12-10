3b2a7802b66c8809277ffb5cac49cf14aa6b4903
Leonid Shevtsov <leonid@shevtsov.me>
2011-12-11 00:31:50 +0200

    prompt and validation

--- lab1.cpp
+++ lab1.cpp
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include "lab1.h"
 
+void printHelp() {
+  printf("This program will show the minimum and maximum elements of an array\n");
+  printf("It will also count the number of occurences when a positive and\n");
+  printf("negative number stand next to each other\n\n");
+}
+
 void readData(int &n, int *&a) {
-  printf("Total number of elements N = ");
-  scanf("%i", &n);
-  
-  // TODO validation
+  do {
+    printf("Total number of integer elements N = ");
+    scanf("%i", &n);
+    if (n <= 0) {
+      printf("N must be greater than 0\n");
+    }
+  } while (n <= 0);
 
   a = new int[n];
 
   for (int i=0; i<n; ++i) {
-    printf("A[%4i] = ", i);
+    printf("A[%2i] = ", i);
     scanf("%i", a+i);
   }
 }
@@ -29,6 +38,7 @@ int main() {
 
   int min, max, neighborsCount;
 
+  printHelp();
   readData(n, a);
   min = findMinElement(n, a);
   max = findMaxElement(n, a);
