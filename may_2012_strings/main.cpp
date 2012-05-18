b30968a6b958c3d103aab2ef7a1bb462105cec81
Leonid Shevtsov <leonid@shevtsov.me>
2012-05-18 09:43:23 +0300

    may 2012 strings

---
+++ may_2012_strings/main.cpp
@@ -0,0 +1,32 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+
+void strcpy_multi(char* dst, const char* src, int count) {
+  char* target = dst;
+  int step = strlen(src);
+  for (int i=0; i<count; ++i) {
+    strcpy(target, src);
+    target += step;
+  }
+}
+
+int main() {
+  char source[80]; // 80 chars should be enough for everybody
+  unsigned int count;
+
+  printf("String: ");
+  scanf("%s", source); 
+  printf("Reps count: ");
+  scanf("%u", &count);
+
+  char* dest = (char*)malloc(strlen(source)*count+1);
+
+  strcpy_multi(dest, source, count);
+
+  printf("Result: %s\n", dest);
+
+  delete dest;
+
+  return 0;
+};
