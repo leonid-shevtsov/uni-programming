b7838f461ea9ac40f58e80900ac1e2a4856cb51d
Leonid Shevtsov <leonid@shevtsov.me>
2012-10-21 21:37:53 +0300

    coursework 1 - refactoring of main.c and result-formatting code

--- course_1/main.c
+++ course_1/main.c
@@ -5,24 +5,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void printBanner() {
+  printf("Not enough arguments\nUsage: passengers [input_filename] [output_filename]\n");
+}
+
+void readData(const char* filename, Passenger** passengers, int* passengers_size) {
+  FILE* input_file = fopen(filename, "r");
+  *passengers = readPassengerFile(input_file, passengers_size);
+  fclose(input_file);
+}
+
+void printResults(const char* filename, Passenger* max_passenger, Flight* max_flight) {
+  FILE* output_file = fopen(filename, "w");
+  fprintf(output_file, "Passenger with max items: %s %s (%i items)\n", max_passenger->name.first_name, max_passenger->name.last_name, max_passenger->items_count);
+  fprintf(output_file, "Flight with maximum weight: %s\n", max_flight->flight_no);
+  printFlight(output_file, max_flight);
+  fclose(output_file);
+}
+
 int main(int argc, const char** argv) {
   if (argc < 3) {
-    printf("Not enough arguments\nUsage: passengers [input_filename] [output_filename]\n");
+    printBanner();
     return -1;
   }
 
-  FILE* input_file = fopen(argv[1], "r");
-  int passengers_size = 0, flights_size=0;
-  Passenger* passengers = readPassengerFile(input_file, &passengers_size);
-  fclose(input_file);
+  Passenger *passengers = NULL, *max_passenger = NULL;
+  Flight *flights = NULL, *max_flight = NULL;
+  int passengers_size = 0, flights_size = 0;
 
-  Flight* flights = mapFlights(passengers, passengers_size, &flights_size);
+  readData(argv[1], &passengers, &passengers_size);
 
-  Flight* maxFlight = flightWithMaxWeight(flights, flights_size);
+  max_passenger = passengerWithMaxItems(passengers, passengers_size);
+  flights = mapFlights(passengers, passengers_size, &flights_size);
+  max_flight = flightWithMaxWeight(flights, flights_size);
 
-  FILE* output_file = fopen(argv[2], "w");
-  printFlight(output_file, maxFlight);
-  fclose(output_file);
+  printResults(argv[2], max_passenger, max_flight);
 
   freeFlights(flights, flights_size);
   freePassengers(passengers, passengers_size);
